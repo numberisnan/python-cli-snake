cc5a2092605a8b9d54984d7bfb1b0ede2f882f42
Faraz Kaleem Malik <43715001+xxxFarazxxx@users.noreply.github.com>
2018-11-13 22:15:06 -0500

    Formatting

--- gameComponents.py
+++ gameComponents.py
@@ -1,7 +1,7 @@
-#Board API
-class Board: #Creates an empty map of pixels; like a canvas
-    def __init__(self,width,height):
-        m = [] #Map array
+# Board API
+class Board:  # Creates an empty map of pixels; like a canvas
+    def __init__(self, width, height):
+        m = []  # Map array
         for i in range(height):
             m.append([])
             for _ in range(width):
@@ -10,7 +10,8 @@ class Board: #Creates an empty map of pixels; like a canvas
         self.width = width
         self.height = height
         self.components = {}
-    def togglePixel(self,x,y,char=1): #Turns specified pixel into specified charcode
+
+    def togglePixel(self, x, y, char=1):  # Turns specified pixel into specified charcode
         "Pixels are zero indexed"
         if x < 0 or y < 0:
             raise IndexError("Out of bounds!")
@@ -18,6 +19,7 @@ class Board: #Creates an empty map of pixels; like a canvas
             raise IndexError("Out of bounds!")
 
         self.board[y][x] = char
+
     def toString(self, score=0):
         "Turns board array into string"
         res = []
@@ -25,90 +27,99 @@ class Board: #Creates an empty map of pixels; like a canvas
         for row in range(self.height):
             res.append(" ".join(map(str, board[row])))
             res.append("\n")
-        return "\n"+ str(score) + "\n" + "".join(res).replace("0","-").replace("1","🐍").replace("2","🍎") #Swap out charcodes for actual chars
+        # Swap out charcodes for actual chars
+        return "\n" + str(score) + "\n" + "".join(res).replace("0", "-").replace("1", "🐍").replace("2", "🍎")
 
-class Component: #A box
-    def __init__(self,x=0,y=0,width=1,height=1,char=1):
+
+class Component:  # A box
+    def __init__(self, x=0, y=0, width=1, height=1, char=1):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.char = char
-    def apply(self,gameboard):
-        gameboard.togglePixel(self.x,self.y, self.char)
+
+    def apply(self, gameboard):
+        gameboard.togglePixel(self.x, self.y, self.char)
+
+
 class Apple(Component):
     eaten = False
-    def newApple(self,gameboard, snake):
+
+    def newApple(self, gameboard, snake):
         import random
         apple = None
         appleCollision = True
-        while appleCollision: #To make sure an apple does not land on the snake
+        while appleCollision:  # To make sure an apple does not land on the snake
             appleCollision = False
-            apple = Apple(random.randint(0,gameboard.width-1),random.randint(0,gameboard.height-1),1,1,2)
+            apple = Apple(random.randint(0, gameboard.width-1),
+                          random.randint(0, gameboard.height-1), 1, 1, 2)
             for i in range(len(snake.body)):
                 if apple.x == snake.body[i].x and apple.y == snake.body[i].y:
                     appleCollision = True
         return apple
-                
 
-class Snake: #Container for conponents, with snakelike methods
-    def __init__(self,direction,length=3,x=10,y=10):
+
+class Snake:  # Container for conponents, with snakelike methods
+    def __init__(self, direction, length=3, x=10, y=10):
         body = []
         for i in range(length):
-            body.append(Component(x-i,y)) #Pastes pixels from right to left, to the front is initially the right end of the snake
+            # Pastes pixels from right to left, to the front is initially the right end of the snake
+            body.append(Component(x-i, y))
         self.body = body
         self.direction = direction
+
     def move(self, apple):
         "The snake is moved by removing the last peice on the snake and adding a new piece to the front"
         d = self.direction
         head = self.body[0]
-        x = head.x #X for peice added to the front
-        y = head.y #Y for peice adde dto the front
+        x = head.x  # X for peice added to the front
+        y = head.y  # Y for peice adde dto the front
 
-
-        if d == "LEFT": #Make coordinates for new peice of the snake
-            x-=1
+        if d == "LEFT":  # Make coordinates for new peice of the snake
+            x -= 1
         elif d == "RIGHT":
-            x+=1
+            x += 1
         elif d == "UP":
-            y-=1
+            y -= 1
         elif d == "DOWN":
-            y+=1
+            y += 1
 
-        if not apple.eaten and head.x == apple.x and head.y == apple.y: #You ate an apple!
-            if d == "LEFT": #Undo the previous move
-                x+=1
+        if not apple.eaten and head.x == apple.x and head.y == apple.y:  # You ate an apple!
+            if d == "LEFT":  # Undo the previous move
+                x += 1
             elif d == "RIGHT":
-                x-=1
+                x -= 1
             elif d == "UP":
-                y+=1
+                y += 1
             elif d == "DOWN":
-                y-=1
-            self.body.insert(0,Component(apple.x,apple.y,1,1))
+                y -= 1
+            self.body.insert(0, Component(apple.x, apple.y, 1, 1))
             apple.eaten = True
         else:
-            for i in range(3,len(self.body)): #Make sure snake does not bite itself
+            for i in range(3, len(self.body)):  # Make sure snake does not bite itself
                 bodypiece = self.body[i]
-                if head.x == bodypiece.x and head.y == bodypiece.y: #Ya bit yerself
+                if head.x == bodypiece.x and head.y == bodypiece.y:  # Ya bit yerself
                     #print(i, ", len: ", len(self.body))
                     raise Exception("Bit yourself!")
 
-            self.body.pop(-1) #Get rid of the last part of the snake
-            self.body.insert(0, Component(x,y,1,1))
+            self.body.pop(-1)  # Get rid of the last part of the snake
+            self.body.insert(0, Component(x, y, 1, 1))
 
-    def addToBody(self,comp):
+    def addToBody(self, comp):
         self.body.append(comp)
-    def apply(self, board, keystate, apple, moveSnake=True): #Draw on board
+
+    def apply(self, board, keystate, apple, moveSnake=True):  # Draw on board
         d = self.direction
         state = keystate["STATE"]
-        
+
         if (d == "UP" and state == "DOWN") or (d == "DOWN" and state == "UP") or (d == "LEFT" and state == "RIGHT") or (d == "RIGHT" and state == "LEFT"):
-            #The snake can only turn, not reverse
+            # The snake can only turn, not reverse
             pass
         else:
-            self.direction = state #Sync the snake's direcion with the keystate
+            self.direction = state  # Sync the snake's direcion with the keystate
 
-        moveSnake and self.move(apple) #Move apple only if specified
+        moveSnake and self.move(apple)  # Move apple only if specified
 
         for i in range(len(self.body)):
             self.body[i].apply(board)
--- main.py
+++ main.py
@@ -4,15 +4,19 @@ Author: Faraz Malik
 Date Created: 10/25/2018
 Last Updated: 10/29/2018
 '''
-try: #Make sure user ran setup.bat so program will have readchar
+try:  # Make sure user ran setup.bat so program will have readchar
     import readchar
 except:
     raise Exception("Please run setup to install required modules")
-import time, threading, random, json
+import time
+import threading
+import random
+import json
 from gameComponents import *
 
 #User configs
-extremeMode = bool(input("Enter 'yes' to play extreme mode, else enter nothing "))
+extremeMode = bool(
+    input("Enter 'yes' to play extreme mode, else enter nothing "))
 gameMode = "extreme" if extremeMode else "normal"
 
 #Settings
@@ -29,14 +33,15 @@ keystate = {
     "UP": False,
     "DOWN": False,
     "LEFT": False,
-    "RIGHT": True, #Snake will initailly be going right
-    "ENTER": False, #Used for debugging
+    "RIGHT": True,  # Snake will initailly be going right
+    "ENTER": False,  # Used for debugging
     "STATE": "RIGHT"
 }
 
-def keydowndetect(): #Thread that updates keystate
+
+def keydowndetect():  # Thread that updates keystate
     global keystate, terminateKeylogger
-    keymapping = { #Maps characters entered to game keystate
+    keymapping = {  # Maps characters entered to game keystate
         "w": "UP",
         "s": "DOWN",
         "a": "LEFT",
@@ -56,63 +61,76 @@ def keydowndetect(): #Thread that updates keystate
                 "STATE": None
             }
             state = keymapping[char]
-            keystate[state] = True #Modifies keystate
+            keystate[state] = True  # Modifies keystate
             if state != "ENTER":
                 keystate["STATE"] = state
     #print("Done")
 
+
 terminateKeylogger = False
 keyloggerThread = threading.Thread(target=keydowndetect)
-keyloggerThread.daemon = True #Make it a daemon thread
-keyloggerThread.start() #Start the keylogger thread
+keyloggerThread.daemon = True  # Make it a daemon thread
+keyloggerThread.start()  # Start the keylogger thread
 
 #Initialise global components
-snake = Snake(keystate["STATE"],3,round(config["board"]["width"]/2), round(config["board"]["height"]/2))
-apple = Apple(0,0,0,0).newApple(Board(config["board"]["width"],config["board"]["height"]), snake)
+snake = Snake(keystate["STATE"], 3, round(
+    config["board"]["width"]/2), round(config["board"]["height"]/2))
+apple = Apple(0, 0, 0, 0).newApple(
+    Board(config["board"]["width"], config["board"]["height"]), snake)
 score = 0
 
 #Animation Loop
-def updateScreen(nextFrame=True): #nextFrame is whether to update all components to the next screen  
+
+
+# nextFrame is whether to update all components to the next screen
+def updateScreen(nextFrame=True):
     global keystate, apple, config, score
-    gameboard = Board(config["board"]["width"],config["board"]["height"]) #Create a new board instance; like a blank canvas
+    # Create a new board instance; like a blank canvas
+    gameboard = Board(config["board"]["width"], config["board"]["height"])
 
     if apple.eaten:
         score += 1
-        apple = apple.newApple(gameboard, snake) #Apple gets reassigned to a new location
+        # Apple gets reassigned to a new location
+        apple = apple.newApple(gameboard, snake)
 
-    apple.apply(gameboard) #Draw the apple onto the board
+    apple.apply(gameboard)  # Draw the apple onto the board
 
-    snake.apply(gameboard, keystate, apple, nextFrame) #Draw the snake onto the board 
-    
-    print(gameboard.toString(score)) #Print out the board
-        
-updateScreen(False) #Draw the fisrt frame
-time.sleep(2) #Wait 2 seconds for game to start
+    # Draw the snake onto the board
+    snake.apply(gameboard, keystate, apple, nextFrame)
+
+    print(gameboard.toString(score))  # Print out the board
+
+
+updateScreen(False)  # Draw the fisrt frame
+time.sleep(2)  # Wait 2 seconds for game to start
 
 while True:
     try:
-        updateScreen() #Will throw an error if game over
+        updateScreen()  # Will throw an error if game over
     except:
         print('You died. Your score is %i' % score)
-        
-        hs_file_r = open("hs.json", 'r') #Turns hs.json into a python dict
+
+        hs_file_r = open("hs.json", 'r')  # Turns hs.json into a python dict
         hs = json.loads(hs_file_r.read())
         hs_file_r.close()
 
         if (hs[gameMode]["score"] < score):
             hs_file_w = open("hs.json", "w")
             print("You made a highscore!")
-            terminateKeylogger = True #Kill the keylogger so you can type your name
-            input("Press enter to continue") #Its a little buggy, so you have to press enter a few times for the logger to terminate
+            terminateKeylogger = True  # Kill the keylogger so you can type your name
+            # Its a little buggy, so you have to press enter a few times for the logger to terminate
+            input("Press enter to continue")
             hs[gameMode]["name"] = input("Name: ")
             hs[gameMode]["score"] = score
-            hs_file_w.write(json.dumps(hs, separators=(',',':')))
+            hs_file_w.write(json.dumps(hs, separators=(',', ':')))
             hs_file_w.close()
         elif hs[gameMode]["score"] == score:
             print("lol just one more point and you would have the highscore.")
-            print("The highscore of " + str(hs[gameMode]["score"]) + " belongs to " + hs[gameMode]["name"])
+            print("The highscore of " +
+                  str(hs[gameMode]["score"]) + " belongs to " + hs[gameMode]["name"])
         else:
-            print("The highscore of " + str(hs[gameMode]["score"]) + " belongs to " + hs[gameMode]["name"])
-        break #Better than sys.exit() or using global variable
-        
-    time.sleep(1/config["fps"]) #Wait before displaying next frame
+            print("The highscore of " +
+                  str(hs[gameMode]["score"]) + " belongs to " + hs[gameMode]["name"])
+        break  # Better than sys.exit() or using global variable
+
+    time.sleep(1/config["fps"])  # Wait before displaying next frame
